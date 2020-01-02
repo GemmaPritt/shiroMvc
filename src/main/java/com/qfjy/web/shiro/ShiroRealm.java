521155ea3b768ad9e10e1df0d1e6b1806cc2d2da
edog <1358512071@qq.com>
2020-01-02 10:45:07 +0800

    first commit

---
+++ src/main/java/com/qfjy/web/shiro/ShiroRealm.java
@@ -0,0 +1,27 @@
+package com.qfjy.web.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 这是自己创建的类，用来存放用户相关数据和角色
+ * 要集成一个借口并实现里面的三个方法
+ */
+public class ShiroRealm implements Realm {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ target/shiro_02/unauth.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/12/20 0020
+  Time: 19:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>无权限页面</title>
+</head>
+<body>
+无权限页面
+</body>
+</html>
